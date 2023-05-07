281ba72e87ea68c8b99da6c8c812846f3242db7f
ElNgatia <ngatiadyte@gmail.com>
2023-05-07 18:19:50 +0300

    wrap if statements in theme_durations in curly brackets

--- lib/theme/theme_durations.dart
+++ lib/theme/theme_durations.dart
@@ -4,44 +4,50 @@ class ThemeDurations {
   ThemeDurations._();
 
   static Duration get menuBackground {
-    if (FlavorConfig.isInTest())
+    if (FlavorConfig.isInTest()) {
       return const Duration(
           milliseconds: 1); //AnimatedWidget cannot have a zero duration
+    }
     return const Duration(seconds: 200);
   }
 
   static Duration get shortAnimationDuration {
-    if (FlavorConfig.isInTest())
+    if (FlavorConfig.isInTest()) {
       return const Duration(
           milliseconds: 1); //AnimatedWidget cannot have a zero duration
+    }
     return const Duration(milliseconds: 200);
   }
 
   static Duration get mediumAnimationDuration {
-    if (FlavorConfig.isInTest())
+    if (FlavorConfig.isInTest()) {
       return const Duration(
           milliseconds: 1); //AnimatedWidget cannot have a zero duration
+    }
     return const Duration(milliseconds: 350);
   }
 
   static Duration get longAnimationDuration {
-    if (FlavorConfig.isInTest())
+    if (FlavorConfig.isInTest()) {
       return const Duration(
           milliseconds: 1); //AnimatedWidget cannot have a zero duration
+    }
     return const Duration(milliseconds: 500);
   }
 
   static Duration get splashAnimationDuration {
-    if (FlavorConfig.isInTest())
+    if (FlavorConfig.isInTest()) {
       return const Duration(
           milliseconds: 1); //AnimatedWidget cannot have a zero duration
+    }
     return const Duration(seconds: 1, milliseconds: 500);
   }
 
   static Duration get demoNetworkCallDuration {
-    if (FlavorConfig.isInTest())
+    if (FlavorConfig.isInTest()) {
       return const Duration(
           milliseconds: 1); //AnimatedWidget cannot have a zero duration
+    }
     return const Duration(milliseconds: 800);
   }
 }
