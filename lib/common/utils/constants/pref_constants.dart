d4cf92d50e5b0cf8d4df25f05882234c141b8c9d
Siro Daves <sirodevske@gmail.com>
2024-12-19 19:52:37 +0300

    minor fixes

--- lib/common/utils/constants/pref_constants.dart
+++ lib/common/utils/constants/pref_constants.dart
@@ -4,17 +4,12 @@ class PrefConstants {
 
   static const isLoggedInKey = 'is_logged_in';
   static const emailKey = 'email_address';
-  static const contractNoKey = 'contract_number';
-  static const erpStaffNoKey = 'erp_staff_number';
-  static const mobileKey = 'mobile_phone';
-  static const nationalIdKey = 'national_id';
   static const userNameKey = 'username';
   static const passWordKey = 'password';
   static const filePathkey = 'file_path';
   static const accessTokenKey = 'access_token';
   static const refreshTokenKey = 'refresh_token';
   static const expiresInKey = 'expires_in';
-
 
   static const dataIsSelectedKey = 'data_selected';
   static const dataIsLoadedKey = 'data_loaded';
