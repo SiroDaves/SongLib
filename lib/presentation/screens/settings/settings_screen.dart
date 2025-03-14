4325d873a18e17f01b21abdeef68456eaab53b05
Siro Daves <sirodevske@gmail.com>
2025-03-14 20:17:34 +0300

    remove settings bloc, it's unneccessary

--- lib/presentation/screens/settings/settings_screen.dart
+++ lib/presentation/screens/settings/settings_screen.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: use_build_context_synchronously
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -11,14 +9,12 @@ import '../../../data/repository/pref_repository.dart';
 import '../../../core/utils/constants/pref_constants.dart';
 import '../../widgets/inputs/radio_input.dart';
 import '../../widgets/progress/custom_snackbar.dart';
-import '../../widgets/progress/general_progress.dart';
 import '../../../core/di/injectable.dart';
 import '../../navigator/route_names.dart';
 import '../../theme/bloc/theme_bloc.dart';
 import '../../theme/theme_data.dart';
 import '../../theme/theme_fonts.dart';
 import '../../theme/theme_styles.dart';
-import '../../blocs/settings/settings_bloc.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -55,71 +51,58 @@ class SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     l10n = AppLocalizations.of(context)!;
-
-    return BlocProvider(
-      create: (context) => SettingsBloc()..add(SettingsInit()),
-      child: BlocConsumer<SettingsBloc, SettingsState>(
-        listener: (context, state) {},
-        builder: (context, state) {
-          var widgets = [
-            Text(
-              l10n.displayTitle,
-              style: TextStyles.headingStyle3,
-            ).padding(left: Sizes.sm, top: Sizes.sm),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.color_lens),
-                title: Text(l10n.appTheme),
-                subtitle: Text('${l10n.appThemeDesc} $appTheme'),
-                onTap: () => selectThemeDialog(context),
-              ),
-            ),
-            Text(
-              l10n.collectionTitle,
-              style: TextStyles.headingStyle3,
-            ).padding(left: Sizes.sm, top: Sizes.sm),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.library_books),
-                title: Text(l10n.reselectSongbooks),
-                subtitle: Text(l10n.reselectSongbooksDesc),
-                onTap: onResetData,
-              ),
-            ),
-            Text(
-              l10n.presentationTitle,
-              style: TextStyles.headingStyle3,
-            ).padding(left: Sizes.sm, top: Sizes.sm),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.slideshow),
-                title: Text(l10n.songPresentation),
-                subtitle: Text(l10n.songPresentationDesc),
-                trailing: Switch(
-                  value: slideVertical,
-                  onChanged: (value) => updateSlideAxis(value),
-                ),
-                onTap: () => updateSlideAxis(!slideVertical),
-              ),
-            ),
-          ];
-
-          return state.maybeWhen(
-            failure: (feedback) => Scaffold(body: EmptyState(title: feedback)),
-            orElse: () => Scaffold(
-              appBar: AppBar(title: Text(l10n.appSettings)),
-              body: ListView.separated(
-                itemCount: widgets.length,
-                physics: const ClampingScrollPhysics(),
-                shrinkWrap: true,
-                separatorBuilder: (_, __) => const SizedBox(height: Sizes.xs),
-                padding: const EdgeInsets.all(Sizes.sm),
-                itemBuilder: (context, index) {
-                  return widgets[index];
-                },
-              ),
-            ),
-          );
+    var widgets = [
+      Text(
+        l10n.displayTitle,
+        style: TextStyles.headingStyle3,
+      ).padding(left: Sizes.sm, top: Sizes.sm),
+      Card(
+        child: ListTile(
+          leading: Icon(Icons.color_lens),
+          title: Text(l10n.appTheme),
+          subtitle: Text('${l10n.appThemeDesc} $appTheme'),
+          onTap: () => selectThemeDialog(context),
+        ),
+      ),
+      Text(
+        l10n.collectionTitle,
+        style: TextStyles.headingStyle3,
+      ).padding(left: Sizes.sm, top: Sizes.sm),
+      Card(
+        child: ListTile(
+          leading: Icon(Icons.library_books),
+          title: Text(l10n.reselectSongbooks),
+          subtitle: Text(l10n.reselectSongbooksDesc),
+          onTap: onResetData,
+        ),
+      ),
+      Text(
+        l10n.presentationTitle,
+        style: TextStyles.headingStyle3,
+      ).padding(left: Sizes.sm, top: Sizes.sm),
+      Card(
+        child: ListTile(
+          leading: Icon(Icons.slideshow),
+          title: Text(l10n.songPresentation),
+          subtitle: Text(l10n.songPresentationDesc),
+          trailing: Switch(
+            value: slideVertical,
+            onChanged: (value) => updateSlideAxis(value),
+          ),
+          onTap: () => updateSlideAxis(!slideVertical),
+        ),
+      ),
+    ];
+    return Scaffold(
+      appBar: AppBar(title: Text(l10n.appSettings)),
+      body: ListView.separated(
+        itemCount: widgets.length,
+        physics: const ClampingScrollPhysics(),
+        shrinkWrap: true,
+        separatorBuilder: (_, __) => const SizedBox(height: Sizes.xs),
+        padding: const EdgeInsets.all(Sizes.sm),
+        itemBuilder: (context, index) {
+          return widgets[index];
         },
       ),
     );
